4f6caa1315de58e4c519fb187fef21e8a943d9d2
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-11-10 18:41:14 +0000

    array map added. post created. css done

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return (
+    <main id="main">
+      {samplePosts.map((samplePost) => {
+        return (
+          <Post key={samplePost.postId}
+                title={samplePost.title} 
+                author={samplePost.author} 
+                date={samplePost.date} 
+                text={samplePost.text} 
+                highlights={samplePost.highlights}
+                img={samplePost.image.link}
+                alt={samplePost.image.alt} />
+        );
+      })}
+      
+    </main>
+  );
 }
 
 export default Main;
